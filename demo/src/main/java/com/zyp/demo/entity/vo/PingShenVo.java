37b7cbb0314cc11015ab6230d1d2f410d48c2af2
zyp20221 <15623377130@163.com>
2024-10-24 15:18:59 +0800

    第六次提交

--- demo/src/main/java/com/zyp/demo/entity/vo/PingShenVo.java
+++ demo/src/main/java/com/zyp/demo/entity/vo/PingShenVo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class PingShenVo {
+    private Long declarationId;
     private String gongHao;
     private String declarationName;
     private String username;
